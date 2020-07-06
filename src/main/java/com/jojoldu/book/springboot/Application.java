bab2d600ab0903a0f7f4d0391d3ae9b0f89b22fb
chws <rebeccachoosir@gmail.com>
2020-07-06 21:21:13 +0900

    refactor : 필요없는 import 문 삭제

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -3,8 +3,6 @@ package com.jojoldu.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.swing.plaf.basic.BasicSplitPaneUI;
-
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,6 +1,5 @@
 package com.jojoldu.book.springboot.web.dto;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
