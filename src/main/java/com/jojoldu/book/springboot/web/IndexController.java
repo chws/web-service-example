2b783b2db1ff893f02c56a304c1be270913d2ff7
chws <rebeccachoosir@gmail.com>
2020-07-19 14:45:12 +0900

    글 등록 버튼과 컨트롤러 연결

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -2,12 +2,19 @@ package com.jojoldu.book.springboot.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+//page 관련 컨트롤러
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
         return "index";
+    } //view resolver가 index.mustache 를 반환하도록 처리해준다
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
