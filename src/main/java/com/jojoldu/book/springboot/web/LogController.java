9ccd3093f275025b05f201f76c599a4187bc643e
chws <rebeccachoosir@gmail.com>
2020-10-10 11:58:59 +0900

    scheduling 적용

--- src/main/java/com/jojoldu/book/springboot/web/LogController.java
+++ src/main/java/com/jojoldu/book/springboot/web/LogController.java
@@ -2,20 +2,33 @@ package com.jojoldu.book.springboot.web;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LogController {
-    private static final Log LOG = LogFactory.getLog( LogController.class );
+
+    private static final Log LOG = LogFactory.getLog(LogController.class);
 
     @RequestMapping("/log")
     @ResponseBody
     public void logExam() {
-        LOG.debug( ">>> debug log" );
-        LOG.info( ">>> info log" );
-        LOG.warn( ">>> warn log" );
-        LOG.error( ">>> error log" );
+        LOG.trace(">>> trace log");
+        LOG.debug(">>> debug log");
+        LOG.info(">>> info log");
+        LOG.warn(">>> warn log");
+        LOG.error(">>> error log");
+    }
+
+    @Scheduled(fixedDelay = 1000)
+    public void logErrorScheduled() {
+        LOG.error(">>> error scheduled");
+    }
+
+    @Scheduled(fixedDelay = 1500)
+    public void logInfoScheduled() {
+        LOG.info(">>> info scheduled");
     }
 }
